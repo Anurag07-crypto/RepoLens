95f920201d4262c9e2c113f84c0f20e0233d7c66
Anurag07-crypto <vanuragkumardesai@gmail.com>
2026-08-23 14:21:28 +0530

    embedding dimentions change

--- Project/indexing/embedding_manager.py
+++ Project/indexing/embedding_manager.py
@@ -25,7 +25,7 @@ class EMBEDDING_MANAGER:
     def __init__(
         self,
         model_name: str = "sentence-transformers/all-MiniLM-L6-v2",
-        fallback_dimension: int = 384,
+        fallback_dimension: int = 768,
     ):
         self.model_name = model_name
         self.client = None
